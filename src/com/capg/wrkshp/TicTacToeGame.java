86c2936a6fd16effc3ff38365bfc4b9de3180810
jindalakhil <akhiljindal222@gmail.com>
2020-10-07 09:13:12 +0530

    [Akhil]Added: add meathod to create tictactoe board

--- src/com/capg/wrkshp/TicTacToeGame.java
+++ src/com/capg/wrkshp/TicTacToeGame.java
@@ -1,7 +1,17 @@
 package com.capg.wrkshp;
 
 public class TicTacToeGame {
+	static char[] board = new char[10];
+	
 	public static void main(String[] args) {
 		System.out.println("welcome to tic tac toe game");
+		TicTacToeGame obj = new TicTacToeGame();
+		obj.createBoard();
+	}
+	
+	private void createBoard() {
+		for(int i=1;i<board.length;i++) {
+			board[i] = ' ';
+		}
 	}
 }
